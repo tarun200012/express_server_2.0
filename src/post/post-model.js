28ff533ffc84e2d0d8f27928d68443a1172417ae
Tarun Gupta <gupta.tarun200012@gmail.com>
2022-08-20 12:49:59 +0530

    added user and post schema

---
+++ src/post/post-model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const postSchema= new mongoose.Schema({
+title:{
+    type:String,
+    required:true
+},
+content:{
+    type: String,
+    minlength: 10
+},
+status: {
+    type: String,
+    enum: ['draft', 'under review', 'published'],
+    default: 'draft'
+},
+user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'user'
+}
+
+},{timestamps:true});
+
+export const Post = mongoose.model('post', postSchema);
